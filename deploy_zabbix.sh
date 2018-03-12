d42b183845684bfe0bd3ba45c64b703b6296f319
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-03-12 11:30:29 +0100

    Adding initial scripts

---
+++ deploy_zabbix.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env ksh
+SOURCE_DIR=$(dirname $0)
+ZABBIX_DIR=/etc/zabbix
+
+LOGSTASH_SERVER=${1:-localhost}
+LOGSTASH_PORT=${2:-9022}
+LOGSTASH_METHOD=${3:-http}
+LOGSTASH_URL="${ELASTIC_METHOD}://${ELASTIC_SERVER}:${ELASTIC_PORT}"
+
+mkdir -p ${ZABBIX_DIR}/scripts/agentd/lostix
+cp ${SOURCE_DIR}/lostix/lostix.conf.example ${ZABBIX_DIR}/scripts/agentd/lostix/lostix.conf
+cp ${SOURCE_DIR}/lostix/lostix.sh ${ZABBIX_DIR}/scripts/agentd/lostix/
+cp ${SOURCE_DIR}/lostix/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/lostix.conf
+sed -i "s|LOGSTASH_URL=.*|LOGSTASH_URL=\"${LOGSTASH_URL}\"|g" ${ZABBIX_DIR}/scripts/agentd/lostix/lostix.conf
---
+++ lostix/lostix.sh
@@ -0,0 +1,147 @@
+#!/usr/bin/env ksh
+PATH=/usr/local/bin:${PATH}
+
+#################################################################################
+
+#################################################################################
+#
+#  Variable Definition
+# ---------------------
+#
+APP_NAME=$(basename $0)
+APP_DIR=$(dirname $0)
+APP_VER="0.0.1"
+APP_WEB="http://www.sergiotocalini.com.ar/"
+TIMESTAMP=`date '+%s'`
+CACHE_DIR=${APP_DIR}/tmp
+CACHE_TTL=5                                      # IN MINUTES
+#
+#################################################################################
+
+#################################################################################
+#
+#  Load Environment
+# ------------------
+#
+[[ -f ${APP_DIR}/${APP_NAME%.*}.conf ]] && . ${APP_DIR}/${APP_NAME%.*}.conf
+
+#
+#################################################################################
+
+#################################################################################
+#
+#  Function Definition
+# ---------------------
+#
+usage() {
+    echo "Usage: ${APP_NAME%.*} [Options]"
+    echo ""
+    echo "Options:"
+    echo "  -a            Query arguments."
+    echo "  -h            Displays this help message."
+    echo "  -j            Jsonify output."
+    echo "  -s ARG(str)   Section (default=stat)."
+    echo "  -v            Show the script version."
+    echo ""
+    echo "Please send any bug reports to sergiotocalini@gmail.com"
+    exit 1
+}
+
+version() {
+    echo "${APP_NAME%.*} ${APP_VER}"
+    exit 1
+}
+
+refresh_cache() {
+    [[ -d ${CACHE_DIR} ]] || mkdir -p ${CACHE_DIR}
+    type=${1:-'node_stats'}
+    file=${CACHE_DIR}/${type}.json
+    if [[ $(( `stat -c '%Y' "${file}" 2>/dev/null`+60*${CACHE_TTL} )) -le ${TIMESTAMP} ]]; then
+	if [[ ${type} == 'node_stats' ]]; then
+	    RESOURCE="_node/stats"
+	fi
+	curl -s "${LOGSTASH_URL}/${RESOURCE}" 2>/dev/null | jq '.' > ${file}
+    fi
+    echo "${file}"
+}
+
+discovery() {
+    resource=${1}
+    json=$(refresh_cache ${resource})
+}
+
+get_stat() {
+    type=${1}
+    name=${2}
+    resource=${3}
+    json=$(refresh_cache ${type})
+    if [[ ${type} =~ ^node_stats$ ]]; then
+        res=`jq -r ".${name}" ${json}`
+    else
+        res=`jq -r ".${type}.\"${name}\".${resource}" ${json}`
+    fi
+    echo ${res}
+}
+#
+#################################################################################
+
+#################################################################################
+while getopts "s::a:s:uphvj:" OPTION; do
+    case ${OPTION} in
+	h)
+	    usage
+	    ;;
+	s)
+	    SECTION="${OPTARG}"
+	    ;;
+        j)
+            JSON=1
+            IFS=":" JSON_ATTR=(${OPTARG//p=})
+            ;;
+	a)
+	    ARGS[${#ARGS[*]}]=${OPTARG//p=}
+	    ;;
+	v)
+	    version
+	    ;;
+         \?)
+            exit 1
+            ;;
+    esac
+done
+
+if [[ ${JSON} -eq 1 ]]; then
+    rval=$(discovery ${ARGS[*]})
+    echo '{'
+    echo '   "data":['
+    count=1
+    while read line; do
+        IFS="|" values=(${line})
+        output='{ '
+        for val_index in ${!values[*]}; do
+            output+='"'{#${JSON_ATTR[${val_index}]:-${val_index}}}'":"'${values[${val_index}]}'"'
+            if (( ${val_index}+1 < ${#values[*]} )); then
+                output="${output}, "
+            fi
+        done 
+        output+=' }'
+        if (( ${count} < `echo ${rval}|wc -l` )); then
+            output="${output},"
+        fi
+        echo "      ${output}"
+        let "count=count+1"
+    done <<< ${rval}
+    echo '   ]'
+    echo '}'
+else
+    if [[ ${SECTION} == 'discovery' ]]; then
+        rval=$(discovery ${ARGS[*]})
+        rcode="${?}"
+    else
+	rval=$( get_stat ${SECTION} ${ARGS[*]} )
+	rcode="${?}"
+    fi
+    echo ${rval:-0} | sed "s/null/0/g"
+fi
+
+exit ${rcode}
